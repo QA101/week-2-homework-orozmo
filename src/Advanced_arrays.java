6c5b57b6c17e4d666a647920928d69a5c7b1955c
orozmo <32623320+orozmo@users.noreply.github.com>
2017-10-24 18:35:09 -0700

    homework 2

--- src/Advanced_arrays.java
+++ src/Advanced_arrays.java
@@ -29,10 +29,10 @@ public class Advanced_arrays {
 	     System.out.println(maxindex);
 		    in.close();
 ----------
-/*
+
  2)Write a program which takes as input 1D- array and finds minimum number, it’s
  index value.
-  */
+
  	
  		int arr_size = 0;
  		int min=0;
@@ -57,7 +57,33 @@ public class Advanced_arrays {
  	    System.out.println(min);
  	    System.out.println(minindex);
  	    in.close();		    
+---------
 
+3) Write a program which takes as input 2D- array and finds maximum number , it's
+index value.
+*/
+		double[][] my2Array = new double[][]{
+			  { 3.5, 1.5, 2.0, 4.6, 7.0},
+			  { 0, 0, 14.7, 2.25, 5.0 },
+			  { 13.3, -77.9, 18.4, -9.32, 12.6}
+			  };
+						 
+					
+			 	double max3 = my2Array[0][0];
+			 	int index3[] = new int [2];
+					for (int i = 0; i < my2Array.length; i++) {
+			        for (int j = 0; j < my2Array[i].length; j++) {
+			        	       if (my2Array[i][j] > max3) {
+		 					max3 = my2Array[i][j];
+				 					index3[0] = i;
+				 					index3[1] = j;
+			 					}
+				        	       
+			 		}
+				}
+						
+		        System.out.print ("Task 3. Maximum number is: "+ max3 + "; ");
+		        System.out.println ("index is: "+ Arrays.toString(index3));
 		
 	}
 
--- src/MimicSignIn.java
+++ src/MimicSignIn.java
@@ -1,94 +1,99 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- * This programming challenge requires the use of string methods, for loops, and nested if statements
- * This program is intended to mimic a log-in string in a terminal window and validation of an email address
- * 
- * This is a challenge exercise and will require time to investigate.
- * 
- * @author dustin
- *
- */
 public class MimicSignIn {
-
-	/**
-	 * Main method should first read arguments from terminal.
-	 * first argument -> username
-	 * second argument -> password
-	 * third argument -> email
-	 * If the number of argument is not equal to 3, the program should exit immediately.
-	 * 
-	 * Then, the program should ask the user to sign in (username and password)
-	 * 
-	 * They have three attempts to log in, otherwise it should exit
-	 * 
-	 * Finally, it should tell them if they have a valid email address
-	 * 
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		/**
-		 *  TODO Check if the arguments is the right length.
-		 *  If not the right length, exit immediately.
-		 */ 
-		if(true) {
-			String username = args[0];
-			String password = args[1];
-			String email = args[2];
-			final int MAX_ATTEMPTS = 3;
-			String username_attempt = "";
-			String password_attempt = "";			
-			Scanner console = new Scanner(System.in);
+public static void main(String[] args) {
+	if(true) {
 			
-			/**
-			 *  TODO write a code block that check if the user supplied the correct username and password
-			 *  You should use a for loop
-			 *  You should trim off spaces in-front of or after the string
-			 *  Then compare the username_attempt to username and password_attempt to password
-			 */
+		final int MAX_ATTEMPTS = 3;
+		
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter your user name, password and e-mail");
+		String str = console.nextLine();
+		String [] list = str.split(" ");
+		String username_attempt = "";
+		String password_attempt = "";	
+		String username = "";
+		String password = "";
+		String email =  "";				
+		if(list.length == MAX_ATTEMPTS) {
+			 username = list[0];
+			 password =  list[1];
+			 email =  list[2];				
+ 			
+ /*
+ 			  TODO write a code block that check if the user supplied the correct username and password
+  			  You should use a for loop
+  			  You should trim off spaces in-front of or after the string
+  			  Then compare the username_attempt to username and password_attempt to password
+   */
 			//for(something){
-				System.out.println("Username:");
-				console.nextLine();
-				
-				System.out.println("Password:");
-				console.nextLine();
-			//}
-			
-			/**
-			 * What is a valid email address?
-			 * 
-			 */
-			if(true) {
-				System.out.println("You have a valid email address");
+			 int attempt = 0;
+			for(int i = 0; i<MAX_ATTEMPTS; i++){		
+				System.out.println("Enter Username:");
+				username_attempt = console.next();
+				System.out.println("Enter Password:");
+				password_attempt = console.next();
+				if(!username.equals(username_attempt)||!password.equals(password_attempt)){
+					System.out.println("Invalid Credentials, try again");
+					attempt++;
+				}
+				if(attempt==0) {
+					i=3;
+				}
+				if(attempt==3) {
+					System.out.println("You exceed the maximum number login attemps");
+					break;
+				}
 			}
-			else {
+  			
+  			/**
+  			 * What is a valid email address?
+  			 * 
+  			 */
+			if(ValidEmail(email)) {
+  				System.out.println("You have a valid email address");
+			
 				//I am dead code because my IF statement is always true. <-- FIX ME
-				System.out.println("You do not have a valid email address");
-			}
-			
-			//closing console
-			console.close();
-		}
-		else {
-			//I am dead code because my IF statement is always true. <-- FIX ME
-			System.out.println("wrong number of elements");
-		}
-	}
-	
-	/**
-	 * This method will take an email string and valid if it has a valid format
-	 * 
-	 * @param email - email string which needs to be valided for cthe correct format
-	 * @return - returns true if the email is valid, returns false if it is not valid
-	 */
-	public static boolean ValidEmail(String email) {
-		if (true) {
-			return true;
-		}
-		else {
-			return false;
+			}	else {				
+  				System.out.println("You do not have a valid email address");
+  			}
+  			
+  			//closing console
+  			console.close();
+  			
+  			//closing console
+  			console.close();
 		}
 		
-	}
-}
+			//I am dead code because my IF statement is always true. <-- FIX ME
+		else {
+ 			System.out.println("wrong number of elements");
+		}
+  	}
+  	
+  	/**
+  	 * This method will take an email string and valid if it has a valid format
+  	 * 
+ -	 * @param email - email string which needs to be valided for cthe correct format
+ +	 * @param email - email string which needs to be validated  for the correct format
+  	 * @return - returns true if the email is valid, returns false if it is not valid
+  	 */
+  	public static boolean ValidEmail(String email)
+  	{
+		if (true) {
+	System.out.println("Enter you email");
+		Scanner console = new Scanner(System.in);
+		if (console.next().equals(email)) {
+			console.close();
+  			return true;
+		}
+		else {
+			console.close();
+  			return false;
+		}
+		
+		}	
+  	}
+  }
